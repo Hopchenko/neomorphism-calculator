c4c98c50b25b51b540d9ba67bb9669a4b859a76f
oleksii.hopchenko@sigma.software <oleksii.hopchenko@sigma.software>
2020-07-24 10:15:47 +0300

    added dark mode switch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "normalize.css"
+
+const themeControls = document.getElementsByName("theme")
+const body = document.getElementsByTagName("body")[0]
+
+themeControls.forEach((control, index) => {
+    control.addEventListener("click", (event) => {
+        if (event.target.value === "dark") {
+            body.classList.remove("light")
+            body.classList.add("dark")
+        } else {
+            body.classList.remove("dark")
+            body.classList.add("light")
+        }
+    })
+})
